e031cb7705ccea681a32524fd486535a65b72b71
krysthal moreno <krysthalmorenocrespo@gmail.com>
2021-10-26 06:53:32 -0300

    second commit

---
+++ dist/pagina3.afd4efe5.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){var t=document.getElementsByClassName("needs-validation");Array.prototype.filter.call(t,(function(t){t.addEventListener("submit",(function(e){!1===t.checkValidity()&&(e.preventDefault(),e.stopPropagation()),t.classList.add("was-validated")}),!1)}))}),!1);
+//# sourceMappingURL=pagina3.afd4efe5.js.map
